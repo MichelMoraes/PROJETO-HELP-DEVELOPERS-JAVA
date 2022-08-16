8d361cde7e6491229911569cea6455db6891c90c
GFT\mlbm <michelmoraes@ext.cvccorp.com.br>
2022-08-16 17:57:28 -0300

    add exemplo funcional

--- help-developers/src/main/java/com/br/devs/java/api/controllers/FunctionalController.java
+++ help-developers/src/main/java/com/br/devs/java/api/controllers/FunctionalController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+
 @RestController
 @RequestMapping(value = "/api/v1/java/")
 @RequiredArgsConstructor
@@ -18,7 +19,21 @@ public class FunctionalController {
     @Operation(summary = "Retorna os detalhes sobre functional programming")
     @GetMapping("functional")
     public String detailFunctional() {
-        return "functional details";
+
+        String detail =
+        "//Antes do Java 8\n"+
+        "List<Integer> itens = Arrays.asList(11, 10, 16, 5, 85);\n"+
+        "for (Integer item : itens) {\n"+
+            "if (item == 16)\n"+
+                "System.out.println(item);\n"+
+        "}"+
+
+        "// Com Java 8 usando Streams e Lambda\n"+
+        "List<Integer> itenss = Arrays.asList(11, 10, 16, 5, 85);\n"+
+
+        "itens.stream().filter(i -> i == 16).forEach(System.out::println);\n";
+
+        return detail;
     }
 
 
