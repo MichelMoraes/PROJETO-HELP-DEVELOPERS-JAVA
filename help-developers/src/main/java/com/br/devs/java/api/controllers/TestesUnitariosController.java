3e71bb5f0f6364b591643d673e4626807c420ff0
Michel Batista de Moraes <micmorba@hotmail.com>
2022-08-24 10:23:22 -0300

    Update TestesUnitariosController.java
    Alteração no return.

--- help-developers/src/main/java/com/br/devs/java/api/controllers/TestesUnitariosController.java
+++ help-developers/src/main/java/com/br/devs/java/api/controllers/TestesUnitariosController.java
@@ -17,7 +17,41 @@ public class TestesUnitariosController {
     @Operation(summary = "Retorna os detalhes sobre Testes Unitários")
     @GetMapping("testes")
     public String detailTestes() {
-        return "Testes Unitários details";
+        return " Exemplo: \n"+
+                "\n" +
+                "@ExtendWith(MockitoExtension.class)\n" +
+                "class CountryControllerTest {\n" +
+                "\n" +
+                "    @InjectMocks\n" +
+                "    CountryController countryController;\n" +
+                "    @Mock\n" +
+                "    CountryService service;\n" +
+                "\n" +
+                "    @BeforeEach\n" +
+                "    void setUp() {\n" +
+                "        MockitoAnnotations.openMocks(this);\n" +
+                "    }\n" +
+                "\n" +
+                "\n" +
+                "    @Test\n" +
+                "    void testeDevePassarQuandoFizerPost(){\n" +
+                "\n" +
+                "        Country country1 = new Country();\n" +
+                "        country1.setId(1);\n" +
+                "        country1.setName(\"BRASIL\");\n" +
+                "\n" +
+                "        MockHttpServletRequest request = new MockHttpServletRequest();\n" +
+                "        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));\n" +
+                "\n" +
+                "        // verifica se o que foi salvo é o mesmo que é esperado no country\n" +
+                "        assertEquals(country1.getName(), countryController.save().getName());\n" +
+                "        assertEquals(country1.getId(), countryController.save().getId());\n" +
+                "\n" +
+                "        // verifica se o método foi chamado pelo menos 1 vez\n" +
+                "        verify(service, atMost(1)).save(country1);\n" +
+                "    }\n" +
+                "\n" +
+                "}";
     }
 
 
